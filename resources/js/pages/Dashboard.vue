927c0bc74f97f307318710c97320fd1675c7bf89
Ignas Nemura <ignas.nemura17@gmail.com>
2025-07-28 20:37:16 +0300

    cleaning up the initial Vue project

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,36 +1,20 @@
-<script setup lang="ts">
+<script setup lang = 'ts'>
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import WorkingHoursForm from '@/components/WorkingHoursForm.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'Dashboard',
+        title: 'Darbo valandų taisyklės',
         href: '/dashboard',
     },
 ];
 </script>
 
 <template>
-    <Head title="Dashboard" />
-
-    <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-            </div>
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                <PlaceholderPattern />
-            </div>
+    <AppLayout :breadcrumbs = 'breadcrumbs'>
+        <div class = 'flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto'>
+            <WorkingHoursForm />
         </div>
     </AppLayout>
 </template>
